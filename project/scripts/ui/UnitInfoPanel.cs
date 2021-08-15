f05c8950cc79a599a705c5637db9e6bd36b58716
sysraccoon <sysraccoon@gmail.com>
2021-08-15 16:05:13 +0300

    fix character info panel

--- project/scripts/ui/UnitInfoPanel.cs
+++ project/scripts/ui/UnitInfoPanel.cs
@@ -6,19 +6,17 @@ public class UnitInfoPanel: Control
     private Label damageLabel;
     private Label moveLabel;
     private Label initiativeLabel;
-    private Label manaControlLabel;
     private Label spellPowerLabel;
     private Label manaLabel;
 
     public override void _Ready()
     {
-        var labels = GetNode<Node>("Labels");
+        var labels = this;
         healthLabel = labels.GetNode<Label>("HealthLabel");
         damageLabel = labels.GetNode<Label>("DamageLabel");
         moveLabel = labels.GetNode<Label>("MoveLabel");
         initiativeLabel = labels.GetNode<Label>("InitiativeLabel");
         manaLabel = labels.GetNode<Label>("ManaLabel");
-        manaControlLabel = labels.GetNode<Label>("ManaControlLabel");
         spellPowerLabel = labels.GetNode<Label>("SpellPowerLabel");
 
         Hide();
@@ -33,21 +31,20 @@ public class UnitInfoPanel: Control
 
         Visible = true;
         var stats = character.BasicStats;
-        healthLabel.Text = $"{stats.Health.Name}: {stats.Health.ActualValue}/{stats.Health.MaxValue}";
-        damageLabel.Text = $"{stats.Damage.Name}: {stats.Damage.ActualValue}";
-        moveLabel.Text = $"{stats.Speed.Name}: {stats.Speed.ActualValue}";
-        initiativeLabel.Text = $"{stats.Initiative.Name}: {stats.Initiative.MinValue}-{stats.Initiative.MaxValue}";
-        manaControlLabel.Text = $"{stats.ManaControl.Name}: {stats.ManaControl.ActualValue}";
-        spellPowerLabel.Text = $"{stats.SpellPower.Name}: {stats.SpellPower.ActualValue}";
+        healthLabel.Text = $"hp: {stats.Health.ActualValue}/{stats.Health.MaxValue}";
+        damageLabel.Text = $"dmg: {stats.Damage.ActualValue}";
+        moveLabel.Text = $"spd: {stats.Speed.ActualValue}";
+        initiativeLabel.Text = $"init: {stats.Initiative.MinValue}-{stats.Initiative.MaxValue}";
+        spellPowerLabel.Text = $"pwr: {stats.SpellPower.ActualValue}";
 
         var spellComponent = character.Components.GetComponent<SpellComponent>();
         if (spellComponent != null)
         {
-            manaLabel.Text = $"Mana: {spellComponent.ManaCount} {spellComponent.ManaType.ToString().ToLower()}";
+            manaLabel.Text = $"mp: {spellComponent.ManaType.ToString().ToLower()} {spellComponent.ManaCount}/{stats.ManaControl.ActualValue}";
         }
         else
         {
-            manaLabel.Text = $"Mana: unavailable";
+            manaLabel.Text = $"mp: unavailable";
         }
     }
 
