9436974a9f376599e3fcf448be1844fd12af2aaa
sysraccoon <sysraccoon@gmail.com>
2021-10-17 19:05:36 +0300

    add character layer remove test

--- project-unity/Assets/Scripts/UnitTests/Map/Layers/CharacterLayerTests.cs
+++ project-unity/Assets/Scripts/UnitTests/Map/Layers/CharacterLayerTests.cs
@@ -30,5 +30,29 @@ namespace UnitTests.Map.Layers
             Assert.AreSame(mockCharacter.Object, characterLayer.GetCharacter(spawnPosition));
             Assert.AreEqual(spawnPosition, characterLayer.GetPosition(mockCharacter.Object));
         }
+
+        [Test]
+        public void TestCharacterRemoveFromLayer()
+        {
+            var mapWidth = 3;
+            var mapHeight = 3;
+
+            var mockMap = new Mock<ILayeredMap>();
+            mockMap.SetupGet(m => m.Height).Returns(mapWidth);
+            mockMap.SetupGet(m => m.Width).Returns(mapHeight);
+            
+            var characterLayer = new CharacterMapLayer();
+            characterLayer.OnAttached(mockMap.Object);
+
+            var mockCharacter = new Mock<ICharacter>();
+            var spawnPosition = new MapCell(0, 0);
+            
+            
+            characterLayer.AddCharacter(mockCharacter.Object, spawnPosition);
+            characterLayer.RemoveCharacter(mockCharacter.Object);
+
+
+            Assert.IsNull(characterLayer.GetCharacter(spawnPosition));
+        }
     }
 }
