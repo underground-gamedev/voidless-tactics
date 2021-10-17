f5aef41935f11debba3486abdcb68b3fce7c0f1a
sysraccoon <sysraccoon@gmail.com>
2021-10-17 22:30:48 +0300

    add simple test for entity stat

---
+++ project-unity/Assets/Scripts/UnitTests/Entities/EntityStatTests.cs
@@ -0,0 +1,18 @@
+using Battle;
+using NUnit.Framework;
+
+namespace UnitTests.Entities
+{
+    public class EntityStatTests
+    {
+        [Test]
+        public void TestUnmodifiedStat()
+        {
+            var baseValue = 10;
+            var stat = new EntityStat(baseValue);
+            
+            Assert.AreEqual(baseValue, stat.Value);
+            Assert.AreEqual(baseValue, stat.BaseValue);
+        }
+    }
+}
