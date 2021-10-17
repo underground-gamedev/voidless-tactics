f8aa22b3ed27251d366e2dc61e7b3ad98467bdd4
sysraccoon <sysraccoon@gmail.com>
2021-10-17 19:02:55 +0300

    add character layer add test

---
+++ project-unity/Assets/Scripts/UnitTests/Map/Layers/CharacterLayerTests.cs
@@ -0,0 +1,34 @@
+using Battle;
+using Battle.Map.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.Map.Layers
+{
+    public class CharacterLayerTests
+    {
+        [Test]
+        public void TestCharacterAddOnLayer()
+        {
+            var mapWidth = 3;
+            var mapHeight = 3;
+
+            var mockMap = new Mock<ILayeredMap>();
+            mockMap.SetupGet(m => m.Height).Returns(mapWidth);
+            mockMap.SetupGet(m => m.Width).Returns(mapHeight);
+            
+            var characterLayer = new CharacterMapLayer();
+            characterLayer.OnAttached(mockMap.Object);
+
+            var mockCharacter = new Mock<ICharacter>();
+            var spawnPosition = new MapCell(0, 0);
+            
+            
+            characterLayer.AddCharacter(mockCharacter.Object, spawnPosition);
+
+
+            Assert.AreSame(mockCharacter.Object, characterLayer.GetCharacter(spawnPosition));
+            Assert.AreEqual(spawnPosition, characterLayer.GetPosition(mockCharacter.Object));
+        }
+    }
+}
