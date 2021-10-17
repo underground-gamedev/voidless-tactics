6cc0585c3b55bec43ab94adb5a5b0a81b70180de
sysraccoon <sysraccoon@gmail.com>
2021-10-17 22:54:51 +0300

    add stat modification test

--- project-unity/Assets/Scripts/Battle/Entities/Characters/EntityStats/StatModifierSource.cs
+++ project-unity/Assets/Scripts/Battle/Entities/Characters/EntityStats/StatModifierSource.cs
@@ -2,6 +2,6 @@ namespace Battle
 {
     public enum StatModifierSource
     {
-        
+        Test,
     }
 }
--- project-unity/Assets/Scripts/UnitTests/Entities/EntityStatTests.cs
+++ project-unity/Assets/Scripts/UnitTests/Entities/EntityStatTests.cs
@@ -1,4 +1,5 @@
 using Battle;
+using Moq;
 using NUnit.Framework;
 
 namespace UnitTests.Entities
@@ -23,6 +24,22 @@ namespace UnitTests.Entities
 
             Assert.AreEqual(baseValue * 2, (stat + baseValue).BaseValue);
             Assert.AreEqual(baseValue * 3, (stat + baseValue * 2).BaseValue);
+        }
+
+        [Test]
+        public void TestStatModification()
+        {
+            var baseValue = 10;
+            var stat = new EntityStat(baseValue);
+
+            var mockModifier = new Mock<EntityStatModifier>();
+            mockModifier.Setup(modifier => modifier.ModifyValue(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(0);
+            
+            stat.AddModifier(StatModifierSource.Test, mockModifier.Object);
+            
+            Assert.AreEqual(baseValue, stat.BaseValue);
+            Assert.AreEqual(0, stat.Value);
         }
     }
 }
