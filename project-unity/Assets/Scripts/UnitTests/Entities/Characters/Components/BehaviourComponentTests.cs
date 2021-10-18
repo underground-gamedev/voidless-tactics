96abd01b7cd294f26cfc2d2d1b4e57db506d0031
sysraccoon <sysraccoon@gmail.com>
2021-10-19 01:02:57 +0300

    remove special test case

--- project-unity/Assets/Scripts/UnitTests/Entities/Characters/Components/BehaviourComponentTests.cs
+++ project-unity/Assets/Scripts/UnitTests/Entities/Characters/Components/BehaviourComponentTests.cs
@@ -6,41 +6,27 @@ namespace UnitTests.Entities.Characters.Components
 {
     public class BehaviourComponentTests
     {
-        public class TestPersonalEvent : IPersonalEvent { }
+        public class FirstTestPersonalEvent : IPersonalEvent { }
 
         public class SecondTestPersonalEvent : IPersonalEvent { }
 
-
-        [Test]
-        public void TestBehaviourEventReaction()
-        {
-            var behCom = new BehaviourComponent();
-            var mockBeh = new Mock<IBehaviour<TestPersonalEvent>>();
-            behCom.Add(mockBeh.Object);
-
-            
-            behCom.Handle(new TestPersonalEvent());
-            
-            
-            mockBeh.Verify(beh => beh.Handle(It.IsAny<TestPersonalEvent>()), Times.Once());
-        }
 
         [Test]
         public void TestMultipleBehavioursImplementation()
         {
             var behCom = new BehaviourComponent();
             var mockBeh = new Mock<IBehaviour>();
-            var firstTestBeh = mockBeh.As<IBehaviour<TestPersonalEvent>>();
+            var firstTestBeh = mockBeh.As<IBehaviour<FirstTestPersonalEvent>>();
             var secondTestBeh = mockBeh.As<IBehaviour<SecondTestPersonalEvent>>();
             
             behCom.Add(mockBeh.Object);
             
             
-            behCom.Handle(new TestPersonalEvent());
+            behCom.Handle(new FirstTestPersonalEvent());
             behCom.Handle(new SecondTestPersonalEvent());
             
             
-            firstTestBeh.Verify(beh => beh.Handle(It.IsAny<TestPersonalEvent>()), Times.Once());
+            firstTestBeh.Verify(beh => beh.Handle(It.IsAny<FirstTestPersonalEvent>()), Times.Once());
             secondTestBeh.Verify(beh => beh.Handle(It.IsAny<SecondTestPersonalEvent>()), Times.Once());
         }
     }
