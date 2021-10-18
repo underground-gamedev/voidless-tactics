75d1a79ff59017c4c5cb66fde429160c4803b1d6
sysraccoon <sysraccoon@gmail.com>
2021-10-19 01:01:57 +0300

    add test for multiple behaviour implementation

--- project-unity/Assets/Scripts/UnitTests/Entities/Characters/Components/BehaviourComponentTests.cs
+++ project-unity/Assets/Scripts/UnitTests/Entities/Characters/Components/BehaviourComponentTests.cs
@@ -7,7 +7,10 @@ namespace UnitTests.Entities.Characters.Components
     public class BehaviourComponentTests
     {
         public class TestPersonalEvent : IPersonalEvent { }
-        
+
+        public class SecondTestPersonalEvent : IPersonalEvent { }
+
+
         [Test]
         public void TestBehaviourEventReaction()
         {
@@ -20,6 +23,25 @@ namespace UnitTests.Entities.Characters.Components
             
             
             mockBeh.Verify(beh => beh.Handle(It.IsAny<TestPersonalEvent>()), Times.Once());
+        }
+
+        [Test]
+        public void TestMultipleBehavioursImplementation()
+        {
+            var behCom = new BehaviourComponent();
+            var mockBeh = new Mock<IBehaviour>();
+            var firstTestBeh = mockBeh.As<IBehaviour<TestPersonalEvent>>();
+            var secondTestBeh = mockBeh.As<IBehaviour<SecondTestPersonalEvent>>();
+            
+            behCom.Add(mockBeh.Object);
+            
+            
+            behCom.Handle(new TestPersonalEvent());
+            behCom.Handle(new SecondTestPersonalEvent());
+            
+            
+            firstTestBeh.Verify(beh => beh.Handle(It.IsAny<TestPersonalEvent>()), Times.Once());
+            secondTestBeh.Verify(beh => beh.Handle(It.IsAny<SecondTestPersonalEvent>()), Times.Once());
         }
     }
 }
