4bd0f26b386d51a29173d666aef67d34f3e7cb9d
sysraccoon <sysraccoon@gmail.com>
2021-11-02 18:35:56 +0300

    relocate map event emitter layer

---
+++ project-unity/Assets/Scripts/VoidLess/Game/Map/MapEventEmitter.cs
@@ -0,0 +1,39 @@
+using System;
+using VoidLess.Core.Entities;
+using VoidLess.Game.Entities.Characters.Components.GlobalEventEmitter;
+using VoidLess.Game.EventSystem.GlobalEvents.UtilityEvents;
+using VoidLess.Game.Map.Base;
+
+namespace VoidLess.Game.Setup.SetupSteps.LogicalSteps
+{
+    public class MapEventEmitter : IMapLayer
+    {
+        private ILayeredMap map;
+
+        private void EmitAddLayer(Type association, IMapLayer layer)
+        {
+            var emitter = map.GetComponent<IGlobalEventEmitter>();
+            emitter?.Emit(new AddMapLayerUtilityEvent(map, association, layer));
+        }
+
+        private void EmitRemoveLayer(Type association, IMapLayer layer)
+        {
+            var emitter = map.GetComponent<IGlobalEventEmitter>();
+            emitter?.Emit(new RemoveMapLayerUtilityEvent(map, association, layer));
+        }
+
+        public void OnAttached(ILayeredMap map)
+        {
+            this.map = map;
+            map.OnLayerAssociated += EmitAddLayer;
+            map.OnLayerUnAssociated += EmitRemoveLayer;
+        }
+
+        public void OnDeAttached()
+        {
+            map.OnLayerAssociated -= EmitAddLayer;
+            map.OnLayerUnAssociated -= EmitRemoveLayer;
+            this.map = null;
+        }
+    }
+}
--- project-unity/Assets/Scripts/VoidLess/Game/Setup/SetupSteps/LogicalSteps/MapLogicalSetupStep.cs
+++ project-unity/Assets/Scripts/VoidLess/Game/Setup/SetupSteps/LogicalSteps/MapLogicalSetupStep.cs
@@ -1,10 +1,7 @@
-using System;
 using UnityEngine;
 using VoidLess.Core.Entities;
 using VoidLess.Game.Entities.Characters.Components.GlobalEventEmitter;
-using VoidLess.Game.EventSystem.GlobalEvents.UtilityEvents;
 using VoidLess.Game.Map;
-using VoidLess.Game.Map.Base;
 using VoidLess.Game.Map.Layers.CharacterMapLayer;
 using VoidLess.Game.Map.Layers.ManaEditorMapLayer;
 using VoidLess.Game.Map.Layers.ManaInfoMapLayer;
@@ -25,7 +22,7 @@ namespace VoidLess.Game.Setup.SetupSteps.LogicalSteps
             state.Map.Set(map);
 
             map.AddWithAssociation<IGlobalEventEmitter>(new GlobalEventEmitter(state.EventQueue));
-            map.AddLayer<MapEventsEmitter>(new MapEventsEmitter());
+            map.AddLayer<MapEventEmitter>(new MapEventEmitter());
                 
             map.AddLayer<ISolidMapLayer>(new NonSolidMapLayer())
                .AddLayer<ICharacterMapLayer>(new CharacterMapLayer())
@@ -35,36 +32,5 @@ namespace VoidLess.Game.Setup.SetupSteps.LogicalSteps
         }
 
         protected override SetupOrder SetupOrder => SetupOrder.MapLogical;
-
-        private class MapEventsEmitter : IMapLayer
-        {
-            private ILayeredMap map;
-
-            private void EmitAddLayer(Type association, IMapLayer layer)
-            {
-                var emitter = map.GetComponent<IGlobalEventEmitter>();
-                emitter?.Emit(new AddMapLayerUtilityEvent(map, association, layer));
-            }
-
-            private void EmitRemoveLayer(Type association, IMapLayer layer)
-            {
-                var emitter = map.GetComponent<IGlobalEventEmitter>();
-                emitter?.Emit(new RemoveMapLayerUtilityEvent(map, association, layer));
-            }
-
-            public void OnAttached(ILayeredMap map)
-            {
-                this.map = map;
-                map.OnLayerAssociated += EmitAddLayer;
-                map.OnLayerUnAssociated += EmitRemoveLayer;
-            }
-
-            public void OnDeAttached()
-            {
-                map.OnLayerAssociated -= EmitAddLayer;
-                map.OnLayerUnAssociated -= EmitRemoveLayer;
-                this.map = null;
-            }
-        }
     }
 }
